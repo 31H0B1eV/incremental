ca211d858e42349725f5e32d114fec3a15e42463
Artem Zinoviev <arzinoviev@gmail.com>
2014-07-01 12:21:45 +0300

    add view all tags and associated with lessons

--- app/controllers/TagsController.php
+++ app/controllers/TagsController.php
@@ -21,41 +21,19 @@ class TagsController extends ApiController {
     }
 
     /**
-	 * Display a listing of the resource.
-	 *
-	 * @return Response
-	 */
-	public function index()
+     * Display a listing of the resource.
+     *
+     * @param null $lessonId
+     * @return Response
+     */
+	public function index($lessonId = null)
 	{
-		$tags = Tag::all();
+        $tags = $this->getTags($lessonId);
 
         return $this->respond([
             'data' => $this->tagTransformer->transformCollection($tags->all())
         ]);
 	}
-
-
-	/**
-	 * Show the form for creating a new resource.
-	 *
-	 * @return Response
-	 */
-	public function create()
-	{
-		//
-	}
-
-
-	/**
-	 * Store a newly created resource in storage.
-	 *
-	 * @return Response
-	 */
-	public function store()
-	{
-		//
-	}
-
 
 	/**
 	 * Display the specified resource.
@@ -68,41 +46,15 @@ class TagsController extends ApiController {
 		//
 	}
 
-
-	/**
-	 * Show the form for editing the specified resource.
-	 *
-	 * @param  int  $id
-	 * @return Response
-	 */
-	public function edit($id)
-	{
-		//
-	}
-
-
-	/**
-	 * Update the specified resource in storage.
-	 *
-	 * @param  int  $id
-	 * @return Response
-	 */
-	public function update($id)
-	{
-		//
-	}
-
-
-	/**
-	 * Remove the specified resource from storage.
-	 *
-	 * @param  int  $id
-	 * @return Response
-	 */
-	public function destroy($id)
-	{
-		//
-	}
+    /**
+     * @param $lessonId
+     * @return \Illuminate\Database\Eloquent\Collection|mixed|static[]
+     */
+    public function getTags($lessonId)
+    {
+        $tags = $lessonId ? Lesson::find($lessonId)->tags : Tag::all();
+        return $tags;
+    }
 
 
 }
--- app/models/Lesson.php
+++ app/models/Lesson.php
@@ -1,8 +1,22 @@
 <?php
 
+/**
+ * Class Lesson
+ */
 class Lesson extends \Eloquent {
 
+    /**
+     * @var array
+     */
     protected $fillable = ['title', 'body'];
 
 //    protected $hidden = ['title'];
+
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsToMany
+     */
+    public function tags()
+    {
+        return $this->belongsToMany('Tag');
+    }
 }
